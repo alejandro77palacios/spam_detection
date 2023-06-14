25b448192bbd3c1eb54a58c416341d948be2fcb5
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2023-06-14 00:00:21 -0600

    Estimator in view

--- classifier/views.py
+++ classifier/views.py
@@ -1,9 +1,24 @@
-from django.http import HttpResponse
+import pickle
+
+from django.conf import settings
 from django.shortcuts import render
 
-from classifier.cleaning import NlpDF, sms
+estimator_path = settings.BASE_DIR / 'classifier' / 'estimator.pkl'
+
+with open(estimator_path, 'rb') as f:
+    classifier = pickle.load(f)
 
 
 def index(request):
-    print(sms)
-    return render(request, 'classifier/index.html')
+    if request.method == 'GET':
+        return render(request, 'classifier/index.html')
+    elif request.method == 'POST':
+        sms = request.POST['input']
+        prediction = classifier.predict([sms])
+        predicted_label = 'Ham' if prediction[0] == 0 else 'Spam'
+        estimated_probabilities = classifier.predict_proba([sms])[0]
+        probability = f'{max(estimated_probabilities):.2%}'
+        context = {'prediction': predicted_label,
+                   'probability': probability,
+                   'sms': sms}
+        return render(request, 'classifier/index.html', context)
