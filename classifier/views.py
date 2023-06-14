6a57289f2c3143530288a77adb3235f297226893
alejandro77palacios <alex_palacios77@ciencias.unam.mx>
2023-06-13 22:00:04 -0600

    Create first template

--- classifier/views.py
+++ classifier/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, you're at the classifier index.")
+    return render(request, 'classifier/index.html')
